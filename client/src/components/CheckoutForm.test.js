80bc473ace4b32416a589f2941d67e766a3a1cab
KSClopton <kyle.clopton@outlook.com>
2020-06-07 16:47:12 -0400

    Finished Tests but the npm test will not run - to look at with TL later

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,42 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    // render the component
+    render (<CheckoutForm />);
+    // query for the formheader
+    const formHeader = screen.getByText(/checkout form/i)
+    // make assertion
+    expect(formHeader).toBeInTheDocument()
+})
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+    // render the component
+    render (<CheckoutForm />);
+    // Fill out the form
+    const firstName = screen.getAllByLabelText(/first name:/i)
+    const lastName = screen.getAllByLabelText(/last name:/i)
+    const address = screen.getAllByLabelText(/address:/i)
+    const city= screen.getAllByLabelText(/city:/i)
+    const state = screen.getAllByLabelText(/state:/i)
+    const zipCode = screen.getAllByLabelText(/zip:/i)
+
+    fireEvent.change(firstName, {target: {value: 'Kyle'}})
+    fireEvent.change(lastName, {target: {value: 'Kyle'}})
+    fireEvent.change(address, {target: {value: 'Kyle'}})
+    fireEvent.change(city, {target: {value: 'Kyle'}})
+    fireEvent.change(state, {target: {value: 'Kyle'}})
+    fireEvent.change(zipCode, {target: {value: 'Kyle'}})
+
+    const submitButton = screen.getByTestId(/checkout/i)
+    // Submit the details
+    fireEvent.click(submitButton)
+    // Query for the success message
+    const successMessage = screen.getByTestId(/successMessage/i)
+    expect(successMessage).toBeInTheDocument();
+    // Assertions
+
+});
